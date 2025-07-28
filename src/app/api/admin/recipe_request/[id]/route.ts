7150eb09f478796de690ffd5711e73ac6979c958
Dagmawit N <21dagmawitnegash@gmail.com>
2025-07-29 00:35:49 +0300

    debug

--- src/app/api/admin/recipe_request/[id]/route.ts
+++ src/app/api/admin/recipe_request/[id]/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getUserFromRequest } from '@/lib/getuser';
 
 export async function PUT(request: NextRequest, context: { params: { id: string } }) {
-  const { id } = context.params;
+  const id  = parseInt(context.params.id);
   const user = await getUserFromRequest(request);
 
   if (!user || !user.isAdmin) {
