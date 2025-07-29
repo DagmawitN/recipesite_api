7f148aced867dfe877665ff2d765b4032609266b
Dagmawit N <21dagmawitnegash@gmail.com>
2025-07-29 15:40:10 +0300

    api/review/id

--- src/app/api/review/[id]/route.ts
+++ src/app/api/review/[id]/route.ts
@@ -1,13 +1,19 @@
 import { prisma } from '@/lib/db';
-import {  NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(
-  req:Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(req: Request) {
+  // Extract recipe id from URL
+  const url = new URL(req.url);
+  const id = url.pathname.split('/').pop();
+  const recipeId = parseInt(id || '');
+
+  if (isNaN(recipeId)) {
+    return NextResponse.json({ error: 'Invalid recipe ID' }, { status: 400 });
+  }
+
   try {
     const reviews = await prisma.review.findMany({
-      where: { recipeId: params.id },
+      where: { recipeId: recipeId },
       include: {
         user: {
           select: { name: true, imageUrl: true },
@@ -17,7 +23,7 @@ export async function GET(
     });
 
     return NextResponse.json(reviews);
-  } catch  {
+  } catch {
     console.error('[GET_REVIEWS_ERROR]');
     return NextResponse.json({ error: 'Server error' }, { status: 500 });
   }
